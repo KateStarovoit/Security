77dd0a233c22c4dae6515272fc77b73c4db88a2b
VolodymyrShabat <57983869+VolodymyrShabat@users.noreply.github.com>
2019-12-17 21:13:32 +0200

    Update Vijener.py

--- Vijener.py
+++ Vijener.py
@@ -1,3 +1,4 @@
+from itertools import cycle
 def encode_vijn(text, n):
     coding_word="drebotiy"
     keytext = ""
@@ -14,7 +15,7 @@ def encode_vijn(text, n):
 
     alp = 'abcdefghijklmnopqrstuvwxyz.,@/ABCDEFGHIJKLMNOPQRSTUVWXYZ~`!#$%^&*()_+-=:.<>?/1234567890'
     f = lambda arg: alp[(alp.index(arg[0])+alp.index(arg[1])%87)%87]
-    return ''.join(map(f, zip(text, keytext)))
+    return ''.join(map(f, zip(text, cycle(keytext))))
 
 
 def decode_vijn(coded_text, n):
@@ -32,7 +33,7 @@ def decode_vijn(coded_text, n):
 
     alp = 'abcdefghijklmnopqrstuvwxyz.,@/ABCDEFGHIJKLMNOPQRSTUVWXYZ~`!#$%^&*()_+-=:.<>?/1234567890'
     f = lambda arg: alp[alp.index(arg[0])-alp.index(arg[1])%87]
-    return ''.join(map(f, zip(coded_text, keytext)))
+    return ''.join(map(f, zip(coded_text, cycle(keytext))))
 
 
 print(encode_vijn("0..58.762.15035.0209216", 12))
